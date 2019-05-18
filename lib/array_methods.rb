b90e53ccb6c0d3edb2cae59b99c38ace8d672115
stupendous-blog-8362 <matthew.peoples@yale.edu>
2019-05-18 17:16:11 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,23 +1,32 @@
 def using_include(array, element)
-
+  if array.include?(element)
+    return true
+  else
+    return false
+  end
 end
 
 def using_sort(array)
-
+  sorted = array.sort
+  return sorted
 end
 
 def using_reverse(array)
-
+  rev = array.reverse
+  return rev
 end
 
 def using_first(array)
-
+  num_one = array.first
+  return num_one
 end
 
 def using_last(array)
-
+  last_one = array.last
+  return last_one
 end
 
 def using_size(array)
-
+  length = array.size
+  return length
 end
